6165633e86f5b754682f6e7de03073485204808e
Nazik <shmnazik79@gmail.com>
2026-06-11 17:42:06 +0530

    feat(taskflow): migrate orchestrator/manager.go to structured slog logging
    # Conflicts:
    #	taskflow/orchestrator/manager.go
    
    # Conflicts:
    #	taskflow/orchestrator/manager.go

---
+++ taskflow/orchestrator/options.go
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2026 Lanka Software Foundation
+
+package orchestrator
+
+import "log/slog"
+
+// Option configures a TaskManager.
+type Option func(*TaskManager)
+
+// WithLogger overrides the structured logger used by TaskManager.
+// Defaults to slog.Default() when not provided.
+func WithLogger(logger *slog.Logger) Option {
+	return func(tm *TaskManager) {
+		tm.logger = logger
+	}
+}
